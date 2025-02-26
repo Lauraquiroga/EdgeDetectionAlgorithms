80f9d64a5d85e2477d526d839b9244ead09b9593
Lauraquiroga <lauragq10@hotmail.com>
2025-02-25 18:51:26 -0800

    Add helper functions and Sobel implementation

--- src/sobel.py
+++ src/sobel.py
@@ -1,5 +1,3 @@
-from matplotlib.image import imread
-import matplotlib.pyplot as plt
 import numpy as np
 
 class Sobel:
@@ -20,5 +18,17 @@ class Sobel:
         image: array representation of the input image in greyscale
         
         """
-        Sobelx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
-        Sobely = np.array([[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]])
+        self.sobelx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
+        self.sobely = np.array([[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]])
+        self.image = image
+
+    def find_edges(self):
+        [rows, columns] = np.shape(self.image)  # we need to know the shape of the input grayscale image
+        sobel_filtered_image = np.zeros(shape=(rows, columns))  # initialization of the output image array (all elements are 0)
+
+        # Now we "sweep" the image in both x and y directions and compute the output
+        for i in range(rows - 2):
+            for j in range(columns - 2):
+                gx = np.sum(np.multiply(self.sobelx, self.image[i:i + 3, j:j + 3]))  # x direction
+                gy = np.sum(np.multiply(self.sobely, self.image[i:i + 3, j:j + 3]))  # y direction
+                sobel_filtered_image[i + 1, j + 1] = np.sqrt(gx ** 2 + gy ** 2)  # calculate the magnitude of the gradient
